5bbf1faf8925b6609c88461cca2aa847861a3bca
rusyln <31722099+rusyln@users.noreply.github.com>
2025-04-07 14:54:26 +0800

    Modal FIx

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-
-<h1>{{ title }}</h1>
-<Modal :header="header" :text="text" theme="sale"/>
-<input type="text" ref="name">
-<button @click="handClick()">Me Click</button>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <button @click="toggleModal">Toggle Modal</button>
+    <!-- Modal will be shown only when showModal is true -->
+    <Modal v-if="showModal" :header="header" :text="text" theme="sale" @close="toggleModal"/>
+    <input type="text" ref="name">
+  </div>
 </template>
 
 <script>
@@ -14,20 +16,19 @@ export default {
   components: {
     Modal
   },
- data(){
-   return {
-     title: 'Hello Vue!',
-     header:'giveaway',
-      text:'text'
-   }
- },
- methods: {
-   handClick(){
-     
-     console.log('Hello'+ name)
-     this.$refs.textbox.clasList.add('active')
-   }
- }
+  data(){
+    return {
+      title: 'Hello Vue!',
+      header: 'Giveaway',
+      text: 'This is a modal text example.',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="backdrop">
-    <div class="modal" :class="{sale: theme ==='sale'}">
-        <h1>{{header}}</h1>
+    <div class="backdrop" @click="closeModal">
+      <div class="modal" :class="{sale: theme === 'sale'}">
+        <h1>{{ header }}</h1>
         <p>{{ text }}</p>
+        <button @click="$emit('close')">Close</button>
+      </div>
     </div>
-  </div>
-</template>
-
-<script>
-export default {
-  props:['header','text', 'theme']
-}
-</script>
-
-<style >
-.modal {
+  </template>
+  
+  <script>
+  export default {
+    props: ['header', 'text', 'theme'],methods: {
+    closeModal(){
+        this.$emit('close');
+    }
+  }
+  }
+  </script>
+  
+  <style>
+  .modal {
     width: 400px;
     padding: 20px;
     margin: 200px auto;
@@ -28,20 +33,16 @@ export default {
     width: 100%;
     height: 100%;
   }
-  .modal h1, p {
-   color: blueviolet;
+  .modal h1,
+  .modal p {
+    color: blueviolet;
   }
   .modal.sale {
-
-background: coral;
-
-color: white;
-
-}
-
-.modal.sale h1 {
-
-color: white
-
-}
-</style>
+    background: coral;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+  </style>
+  
